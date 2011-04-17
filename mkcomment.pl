f91acac7aca9279db5e0358511261ba39f0e692f
M. Nunberg <mordy@mpb.local>
2011-04-17 12:38:56 +0100

    first comment

---
+++ mkcomment.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Text::Wrap;
+my $width = 80;
+my $style = "pxx";
+my $hpad = 3;
+my $wpad = 3;
+my $txt = "This is foo";
+
+my $help = <<EOM;
+USAGE: $0 [--width=80 --hpad=3 --wpad=3 --style=pxx] <message>
+	--width: wrap at width
+	--hpad: provide HPAD lines above and below header
+	--wpad: provide comment char WPAD times on the beginning and end of line
+	--style: comment style, pxx or cpp
+EOM
+
+if (grep $_ =~ /^-h|--help$/, @ARGV) {
+	print STDERR $help;
+	exit(1);
+}
+GetOptions(
+	"width=i" => \$width,
+	"style=s" => \$style,
+	"hpad=i" => \$hpad,
+	"wpad=i" => \$wpad,
+);
+
+my $COMMENT_CHAR;
+if ($style =~ /pxx/i) {
+	$COMMENT_CHAR = '#';
+} else {
+	$COMMENT_CHAR='/';
+	if ($wpad < 2) {
+		$wpad = 2;
+	}
+}
+
+my $m_width = $width;
+$m_width -= $wpad * 2; #Remove for padding on each side..
+$m_width -= 2; #for space padding
+
+$Text::Wrap::columns = $m_width;
+
+my $message = pop(@ARGV);
+if ($message && $message eq '-' || !$message) {
+	$message = join("", <>);
+}
+$message = wrap("", "", $message);
+my @lines = map { 
+		my $l = $_;
+		$l .= " " x ($m_width - length($l));
+		$l = sprintf("%s %s %s", $COMMENT_CHAR x $wpad, $l, $COMMENT_CHAR x $wpad);
+		$l;
+	}
+	split(/\n/, $message);
+
+my $hdrftr = ($COMMENT_CHAR x $width);
+foreach my $i (1..$hpad) {
+	push @lines, $hdrftr;
+	unshift @lines, $hdrftr;
+}
+
+$message = join("\n", @lines);
+print $message;
